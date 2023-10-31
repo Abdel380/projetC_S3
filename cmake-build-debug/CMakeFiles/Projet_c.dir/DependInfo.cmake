89c3d15a0723738ae7805a50c884348c33b7be8f
AlexVzq7 <alexandre.vazquez@efrei.net>
2023-10-31 23:36:29 +0100

    Changement create_list

---
+++ cmake-build-debug/CMakeFiles/Projet_c.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/alex/CLionProjects/projetC_S32/cell.c" "CMakeFiles/Projet_c.dir/cell.c.obj" "gcc" "CMakeFiles/Projet_c.dir/cell.c.obj.d"
+  "C:/Users/alex/CLionProjects/projetC_S32/fonctions.c" "CMakeFiles/Projet_c.dir/fonctions.c.obj" "gcc" "CMakeFiles/Projet_c.dir/fonctions.c.obj.d"
+  "C:/Users/alex/CLionProjects/projetC_S32/list.c" "CMakeFiles/Projet_c.dir/list.c.obj" "gcc" "CMakeFiles/Projet_c.dir/list.c.obj.d"
+  "C:/Users/alex/CLionProjects/projetC_S32/main.c" "CMakeFiles/Projet_c.dir/main.c.obj" "gcc" "CMakeFiles/Projet_c.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Projet_c.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_c.dir/cell.c.obj"
+  "CMakeFiles/Projet_c.dir/cell.c.obj.d"
+  "CMakeFiles/Projet_c.dir/fonctions.c.obj"
+  "CMakeFiles/Projet_c.dir/fonctions.c.obj.d"
+  "CMakeFiles/Projet_c.dir/list.c.obj"
+  "CMakeFiles/Projet_c.dir/list.c.obj.d"
+  "CMakeFiles/Projet_c.dir/main.c.obj"
+  "CMakeFiles/Projet_c.dir/main.c.obj.d"
+  "Projet_c.exe"
+  "Projet_c.exe.manifest"
+  "Projet_c.pdb"
+  "libProjet_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
