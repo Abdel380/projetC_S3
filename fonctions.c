083c18724f113e55d044d6396822e6c7a7dd1626
AlexVzq7 <alexandre.vazquez@efrei.net>
2023-11-09 11:52:38 +0100

    Début partie 3

--- fonctions.c
+++ fonctions.c
@@ -4,4 +4,56 @@
 
 #include "fonctions.h"
 
+DATE* initialise_date(){
+    DATE * date = malloc(sizeof(DATE));
+    return date;
+}
 
+void saisir_date(DATE * date){
+    date = initialise_date(); // alloue la memoire
+
+
+    printf("En quelle annee est votre rdv :"); // assignation pour l'annee
+    while ( date->annee < 2023 ){
+        scanf("%d", &(date->annee));
+    }
+    printf("\n");
+
+
+    printf("En quelle mois est votre rdv :"); // assignation pour le mois
+    while ( date->mois < 0 || date->mois > 12 ){
+        scanf("%d", &(date->mois));
+    }
+    printf("\n");
+
+
+    int max;
+    if ( date->mois == 1 || date->mois == 3 || date->mois == 5 || date->mois == 7 || date->mois == 8 || date->mois == 10 || date->mois == 12){
+        max = 31;
+    }
+    else if ( date->mois == 2){
+        max = 29;
+    }
+    printf("Quelle jour est votre rdv :");
+    while ( date->jour < 0 || date->jour > max){
+        scanf("%d", &(date->jour));
+    }
+
+
+
+    return;
+}
+
+CONTACT * initialise_contact(){
+    CONTACT * contact = malloc(sizeof(CONTACT));
+
+    return contact;
+}
+
+void saisir_contact(CONTACT * contact){
+    contact = initialise_contact();
+    printf("Entrez votre nom :");
+    scanf("%s", contact->nom);
+    printf("\n");
+    
+}
--- fonctions.h
+++ fonctions.h
@@ -7,8 +7,45 @@
 
 #include "list.h"
 
+typedef struct{
+    char * nom;
+    char * prenom;
+}CONTACT;
 
+typedef struct{
+    int jour;
+    int mois;
+    int annee;
+}DATE;
 
+typedef struct{
+    int heure;
+    int minutes;
+}RDV_HORAIRE;
+
+typedef struct{
+    int heure;
+    int minutes;
+}RDV_DUREE;
+
+typedef struct{
+    char * objet;
+}RDV_OBJET;
+
+typedef struct{
+    DATE  d;
+    RDV_HORAIRE  horaire;
+    RDV_DUREE duree;
+    RDV_OBJET objet;
+}RDV;
+
+DATE* initialise_date();
+
+void saisir_date(DATE * );
+
+CONTACT * initialise_contact();
+
+void saisir_contact(CONTACT * );
 
 
 
